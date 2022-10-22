18d6967a3d16842c0fcfda98d28fa5a6e69ced1e
AngieLeón <115847720+AngelicaLeonR@users.noreply.github.com>
2022-10-21 22:00:52 -0500

    Creación de plantilla y vistas
    Creación de una plantilla con barra de navegación y las vistas correspondientes a formulario y tabla

---
+++ resources/views/Tabla.blade.php
@@ -0,0 +1,9 @@
+@extends('Plantilla')
+
+@section('contenido')
+
+<h1 class="display-4 text-center mt-5 mb-5"> Tabla</h1>
+
+
+
+@stop
---
+++ resources/views/plantilla.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Plantilla</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!--te falta boostrap-->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+</head>
+<body>
+    <!--Barra de navegación-->
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">PracticaGitHub</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="Formulario">Formulario</a>
+            <a class="nav-link" href="Tabla">Tabla</a>
+            
+        </div>
+        </div>
+    </div>
+</nav> 
+
+<!--Contenido de la pagina-->
+
+@yield('contenido')
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
