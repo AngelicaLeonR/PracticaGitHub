c8d0b523d144ae51f741b2080c227474cc42b18c
AngieLeón <115847720+AngelicaLeonR@users.noreply.github.com>
2022-10-21 22:19:32 -0500

    Rutas
    Creación de las rutas para las vistas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('Formulario', function () {
+    return view('Formulario');
+});
+
+Route::get('Tabla', function () {
+    return view('Tabla');
+});
+
